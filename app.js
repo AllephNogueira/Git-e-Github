2692978c43787d6ad6d295048a76b12ca0e4c294
Alleph Nogueira <allephn@hotmail.com.br>
2024-03-14 17:48:23 -0300

    Atualizamos o valor maximo para 1000

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 // Atualizando dados novamente
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 1000; // Atualizando numeros para contar de 1 a 1000
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
